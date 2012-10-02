cca83184313c133dd34ee5c12121098db442e728
Gergely Topolyai <gerely01@gmail.com>
2012-10-02 15:14:33 +0200

    last URL history added

---
+++ src/main/java/com/github/urlchopper/domain/ShortUrlDTO.java
@@ -0,0 +1,24 @@
+package com.github.urlchopper.domain;
+
+public class ShortUrlDTO {
+
+    private String originalUrl;
+    private String shortUrl;
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
+
+}
