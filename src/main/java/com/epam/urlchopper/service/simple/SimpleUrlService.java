f85df130233c7067788e59f79173a802d49ca2c9
Marton Sereg <martonsereg@gmail.com>
2012-10-10 11:07:08 +0200

    additional url schemes are now accepted

--- src/main/java/com/epam/urlchopper/service/simple/SimpleUrlService.java
+++ src/main/java/com/epam/urlchopper/service/simple/SimpleUrlService.java
@@ -165,10 +165,17 @@ public class SimpleUrlService implements UrlService {
 
     private String convertToValidUrl(String originalUrl) {
         String ret = originalUrl;
-        if (!originalUrl.startsWith("http://")) {
+        if (!originalUrlIsValid(originalUrl)) {
             ret = "http://" + originalUrl;
         }
         return ret;
+    }
+
+    private boolean originalUrlIsValid(String originalUrl) {
+        return originalUrl.startsWith("http://") || originalUrl.startsWith("https://") || originalUrl.startsWith("ftp://") || originalUrl.startsWith("mailto:")
+                || originalUrl.startsWith("gopher://") || originalUrl.startsWith("telnet://") || originalUrl.startsWith("mid:") || originalUrl.startsWith("cid:")
+                || originalUrl.startsWith("news:") || originalUrl.startsWith("nntp://") || originalUrl.startsWith("tn3270://") || originalUrl.startsWith("rlogin://")
+                || originalUrl.startsWith("file://") || originalUrl.startsWith("wais://") || originalUrl.startsWith("nfs://") || originalUrl.startsWith("prospero://");
     }
 
     private String generateShortUrl() {
